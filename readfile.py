3a830e155be842dff98a3efb381104e99be27927
wei313511022 <tommy1213.chen@gmail.com>
2024-10-14 15:54:27 +0800

    hi

--- readfile.py
+++ readfile.py
@@ -1,6 +1,24 @@
 import pandas as pd
-from sklearn.preprocessing import OneHotEncoder
-import numpy as np 
+import numpy as np
+
+def one_hot_encode(data,name):
+    
+    max_val = int(max(data))
+    min_val = int(min(data))
+    length = max_val - min_val
+    one_hot_matrix = np.zeros((len(data.axes[0]), length+1), dtype=float)
+    
+    for i, value in enumerate(data):
+        one_hot_matrix[i, int(value) - min_val] = 1
+    
+    encode = pd.DataFrame(one_hot_matrix)
+    columns_name = []
+    for i in range(min_val ,max_val+1):
+        columns_name.append(name+"_"+str(i))
+    encode.columns = columns_name
+
+    return encode
+    
 
 def read_file(file_path):
     data = pd.read_csv(file_path)
@@ -14,26 +32,15 @@ def read_file(file_path):
     #Here we extract the columns with object datatype as they are the categorical columns
     categorical_columns = ['Orientation','Glazing Area Distribution']
     
-    #Initialize OneHotEncoder
-    encoder = OneHotEncoder(sparse_output=False)
-
-    # Apply one-hot encoding to the categorical columns
-    one_hot_encoded = encoder.fit_transform(df[categorical_columns])
-
-    #Create a DataFrame with the one-hot encoded columns
-    #We use get_feature_names_out() to get the column names for the encoded data
-    one_hot_df = pd.DataFrame(one_hot_encoded, columns=encoder.get_feature_names_out(categorical_columns))
-
-    # Concatenate the one-hot encoded dataframe with the original dataframe
-    df_encoded = pd.concat([df, one_hot_df], axis=1)
-
+    for i in categorical_columns:
+        encode = one_hot_encode(df[i],i)
+        df = pd.concat([df, encode], axis=1)
+    df_encoded = df.drop(categorical_columns, axis=1)
     # Drop the original categorical columns
-    df_encoded = df_encoded.drop(categorical_columns, axis=1)
 
     # Display the resulting dataframe
     # print(f"Encoded Employee data : \n{df_encoded}")
-    temp = df_encoded.drop('Heating Load', axis=1)
-    x = np.array(temp)
+    x = df_encoded.drop('Heating Load', axis=1)
     y = df_encoded['Heating Load']
     
     return x,y
