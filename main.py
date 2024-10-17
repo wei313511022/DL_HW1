10d9d8c88ecc2480b569aad0ef451e6b3ab3c1f7
wei313511022 <tommy1213.chen@gmail.com>
2024-10-17 21:41:06 +0800

    1

--- main.py
+++ main.py
@@ -1,22 +1,11 @@
 import readfile as rd
 import neuralnetwork as nn
-import numpy as np 
 import matplotlib.pyplot as plt
 
 file = '2024_energy_efficiency_data.csv'
 train_ratio = 0.75
 
 x_train,y_train,x_test,y_test,y_train_mean,y_train_std,y_test_mean,y_test_std = rd.read_file(file,train_ratio)
-print(f"x_train :\n {x_train.shape}")
-print(f"y_train :\n {y_train.shape}")
-print(f"x_test :\n {x_test.shape}")
-print(f"y_test :\n {y_test.shape}")
-# layers = [x.shape[1], 64, 32, 1]
-# learning_rate=0.01
-# epochs=100
-# batch_size=32
-
-# trained_parameters = nn.mini_batch_SGD(x, y, layers, learning_rate, epochs, batch_size)
 
 # Define the network
 input_size = 17
@@ -34,11 +23,8 @@ loss_record = network.train(x_train, y_train, epochs, learning_rate, y_train_mea
 # print(x_test[0].reshape(1,17)*x_test_std+x_test_mean)
 output = network.forward(x_test)*y_test_std+y_test_mean
 y = y_test*y_test_std+y_test_mean
-# print(network.weights)
-# print("Predicted Output:")
-# print(output)
-numbers = range(1,193)
 
+numbers = range(1,193)
 plt.plot(numbers, output, color = 'red', label = 'predict')
 plt.plot(numbers, y, color = 'blue', label = 'y_test')
 plt.xlabel('case')
